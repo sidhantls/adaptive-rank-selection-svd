ca7fc861e7105e3f5730f272ca295a041e881f2d
Sidhant <sidhant96@outlook.com>
2024-10-21 20:46:16 +0530

    multigpu

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -1,5 +1,6 @@
 import torch.nn.functional as F
 from tqdm import tqdm
+import torch
 
 def configure_required_grad(model):
     """
@@ -38,3 +39,27 @@ def count_parameters(model):
     total_params = sum(p.numel() for p in model.parameters())
     total_params_in_billion = total_params / 1e9
     return total_params_in_billion
+
+def push_to_multi_gpu(model):
+    """
+    Pushes MLP layers with numbers between 3 and 20 in their names to one GPU (device 1),
+    and the rest of the layers to another GPU (device 0).
+    """
+
+    if torch.cuda.is_available() and torch.cuda.device_count() >= 2:
+        device0 = torch.device("cuda:0")
+        device1 = torch.device("cuda:1")
+    else:
+        raise RuntimeError("At least two CUDA devices are required for this operation.")
+
+    # Push modules to the corresponding devices
+    for name, module in model.named_modules():
+        # Check if 'mlp' is in the name and if any number between 3 and 20 is present
+        if  'gate_proj' in name or 'up_proj' in name:
+            module.to(device1)  # Push to device 1
+            print(f"{name} pushed to device 1 (cuda:1)")
+        else:
+            module.to(device0)  # Push to device 0
+            print(f"{name} pushed to device 0 (cuda:0)")
+
+    return model
