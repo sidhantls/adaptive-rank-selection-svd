0ad1b30f130c43d8978fae39d584306532ec68ac
sidhantls <sidhant96@outlook.com>
2025-02-06 12:55:53 +0000

    add sample commands for simple layer

---
+++ commands.sh
@@ -0,0 +1,18 @@
+# Define variables
+MODEL="meta-llama/Llama-2-7b-hf"
+BS=2
+lr=1e-2
+TRAIN_SAMPLES=70000
+cache_dir="train_cache"
+RATIO=0.85
+lambda_scale=0
+gamma_scale=1
+beta_scale=0.70
+layer_type="simple"
+r_loss="simple"
+
+# Construct exp_name
+exp_name="${MODEL##*/}_lambda${lambda_scale}_gamma${gamma_scale}_beta${beta_scale}_${layer_type}"
+
+python train_adaptive.py --model_name=$MODEL --batch_size=$BS --num_train_samples=$TRAIN_SAMPLES --max_length=256 --lr=$lr --eval_freq_steps=2000 --exp_name=$exp_name --cache_dir=$cache_dir --eval_full --act_aware="activation" --targe_param_ratio=$RATIO --lambda_scale=$lambda_scale --gamma_scale=$gamma_scale --layer_type=$layer_type --r_loss=$r_loss --beta_scale=$beta_scale
+
